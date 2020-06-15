b05e35862e213bbf00834fb67b0815b212954349
EidorianAvi <reneavila1993@gmail.com>
2020-06-15 14:11:32 -0600

    Create Backend create models create migrations

---
+++ cooking-backend/db/migrate/20200615195950_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.integer :ready_in_minutes
+      t.integer :servings
+      t.string :recipe_url
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
